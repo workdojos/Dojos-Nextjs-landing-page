41a5aaff0195c756daaa99d390fe2e2e5060a248
viewprox86 <workmates.live@outlook.com>
2023-10-26 16:05:52 -0500

    sdf

--- src/app/components/DojosSection3.jsx
+++ src/app/components/DojosSection3.jsx
@@ -7,110 +7,110 @@ import { motion, useInView } from "framer-motion";
 const dojosData = [
   {
     id: 1,
-    title: "Account Executive",
+    title: "Growers",
     description: "As an Account Executive, leverage open-source CRM software to seamlessly manage client relationships, track sales activities, and analyze data, enhancing sales effectiveness and productivity.",
-    image: "/images/projects/accountexecutive.jpg",
-    tag: ["All", "Sales"],
-    gitUrl: "https://og.admins.live/#Account-Executives-Dojo",
+    image: "/images/projects/growers.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Growers",
     previewUrl: "/",
   },
   {
     id: 2,
-    title: "Administrators",
+    title: "Doctors",
     description: "Open-source technology can be used by administrators to create a collaborative task management platform, allowing teams to efficiently coordinate and track their progress while fostering a sense of shared responsibility and engagement.",
-    image: "/images/projects/administrator.jpg",
-    tag: ["All", "Administration"],
-    gitUrl: "https://og.admins.live/#Administrators",
+    image: "/images/projects/clinician.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Doctors",
     previewUrl: "/",
   },
   {
     id: 3,
-    title: "Car Dealers",
+    title: "Explorers",
     description: "Use open-source technology to create a virtual reality showroom for car dealers, allowing customers to explore and customize vehicles from the comfort of their homes.",
-    image: "/images/projects/dealerships.jpg",
-    tag: ["All", "Sales"],
-    gitUrl: "https://og.admins.live/#Car-Dealers",
+    image: "/images/projects/explorers.jpg",
+    tag: ["All", "Travel"],
+    gitUrl: "https://og.admins.live/#Explorers",
     previewUrl: "/",
   },
   {
     id: 4,
-    title: "Clerks",
+    title: "Life Coaches",
     description: "As a clerk, you can utilize open-source technology to automate document management and streamline administrative tasks, improving efficiency and reducing manual workload.",
-    image: "/images/projects/clerk.jpg",
-    tag: ["All", "Sales"],
-    gitUrl: "https://og.admins.live/#Clerks",
+    image: "/images/projects/lifecoaches.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Life-Coaches",
     previewUrl: "/",
   },
   {
     id: 5,
-    title: "Digital Marketers",
+    title: "Neighbors",
     description: "Utilizing open-source social media analytics tools, digital marketers can gain valuable insights on audience behavior, optimize campaigns, and measure the impact of their strategies, all while saving costs and fostering collaboration within the marketing team.",
-    image: "images/projects/digitalmarketers.jpg",
-    tag: ["All", "Marketing"],
-    gitUrl: "https://og.admins.live/#Digital-Marketers",
+    image: "images/projects/neighborly.jpg",
+    tag: ["All", "Public Services"],
+    gitUrl: "https://og.admins.live/#Neighbors",
     previewUrl: "/",
   },
   {
     id: 6,
-    title: "Lead Generators",
+    title: "Park Rangers",
     description: "Open-source technology can be used by lead generators to build a customizable chatbot that automates lead qualification and captures valuable customer information, enhancing efficiency and improving lead generation efforts.",
-    image: "/images/projects/leadgenerator.jpg",
-    tag: ["All", "Marketing"],
-    gitUrl: "https://og.admins.live/#Lead-Generators",
+    image: "/images/projects/parkranger.jpg",
+    tag: ["All", "Public Services"],
+    gitUrl: "https://og.admins.live/#Park-Rangers",
     previewUrl: "/",
   },
   {
     id: 7,
-    title: "Correspondents",
+    title: "Pharmacists",
     description: "Open-source technology can be utilized by news correspondents to create an AI-powered fact-checking tool that automatically verifies information in real-time, enabling accurate reporting and combating misinformation.",
-    image: "/images/projects/correspondents.jpg",
-    tag: ["All", "Communications"],
-    gitUrl: "https://og.admins.live/#Correspondents",
+    image: "/images/projects/pharmacists.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Pharmacists",
     previewUrl: "/images/projects/academies.jpg",
   },
   {
     id: 8,
-    title: "Public Relations",
+    title: "Physical Therapists",
     description: "PR Representative: Utilizing open-source technology, create a dynamic media monitoring dashboard that consolidates real-time news mentions and social media analytics, enabling PR representatives to track and analyze brand sentiment and campaign effectiveness more efficiently.",
-    image: "/images/projects/publicrelations.jpg",
-    tag: ["All", "Communications"],
-    gitUrl: "https://og.admins.live/#Public-Relations",
+    image: "/images/projects/physicaltherapist.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Physical-Therapists",
     previewUrl: "/",
   },
   {
     id: 9,
-    title: "Project Managers",
+    title: "Nurses",
     description: "Open-source project management software provides collaborative task tracking and real-time updates, empowering project managers to efficiently coordinate teams and streamline project workflows.",
-    image: "/images/projects/projectmanagers.jpg",
-    tag: ["All", "Administration"],
-    gitUrl: "https://og.admins.live/#Project-Managers",
+    image: "/images/projects/registerednurse.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Nurses",
     previewUrl: "/",
   },
   {
     id: 10,
-    title: "Retailers",
+    title: "Social Workers",
     description: "Open-source technology can be used by retailers to create personalized shopping experiences through AI-powered recommendation systems, enhancing customer satisfaction and boosting sales.",
-    image: "/images/projects/retailers.jpg",
-    tag: ["All", "Sales"],
-    gitUrl: "https://og.admins.live/#Retailers",
+    image: "/images/projects/socialworker.jpg",
+    tag: ["All", "Public Services"],
+    gitUrl: "https://og.admins.live/#Social-Workers",
     previewUrl: "/",
   },
   {
     id: 11,
-    title: "Sales Coaches",
+    title: "Travel Agents",
     description: "Create an open-source virtual sales training platform that combines interactive simulations and AI-powered coaching to enhance sales coaching effectiveness, empowering sales coaches to train and mentor their teams remotely with engaging, personalized experiences.",
-    image: "images/projects/salescoaches.jpg",
-    tag: ["All", "Sales"],
-    gitUrl: "https://og.admins.live/#Sales-Coaches",
+    image: "images/projects/travelagents.jpg",
+    tag: ["All", "Travel"],
+    gitUrl: "https://og.admins.live/#Travel-Agents",
     previewUrl: "/",
   },
   {
     id: 12,
-    title: "Telemarketers",
+    title: "Yoga Instructors",
     description: "Open-source technology can be utilized by telemarketers to create a personalized voice-bot assistant that dynamically adapts its responses based on caller preferences, enhancing customer engagement and satisfaction. This intelligent system can be further integrated with CRM platforms and analytics tools to optimize lead conversion rates and improve overall telemarketing performance.",
-    image: "/images/projects/telemarketer.jpg",
-    tag: ["All", "Sales"],
-    gitUrl: "https://og.admins.live/#Telemarketers",
+    image: "/images/projects/yogainstructor.jpg",
+    tag: ["All", "Health & Wellness"],
+    gitUrl: "https://og.admins.live/#Yoga-Instructors",
     previewUrl: "/",
   },
 ];
@@ -136,31 +136,26 @@ const DojosSection = () => {
   return (
     <section id="dojos">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        Grow your Business
+        Health & Happiness
       </h2>
       <div className="text-white flex flex-flow justify-center items-center gap-2 pb-8">
         <ProjectTag
           onClick={handleTagChange}
-          name="Marketing"
+          name="Health & Wellness"
           isSelected={tag === "Marketing"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="Administration"
-          isSelected={tag === "Administration"}
         />
         </div>
         <div className="text-white flex flex-flow justify-center items-center gap-2 pb-8">
         
         <ProjectTag
           onClick={handleTagChange}
-          name="Communications"
-          isSelected={tag === "Communications"}
+          name="Travel"
+          isSelected={tag === "Travel"}
         />
         <ProjectTag
           onClick={handleTagChange}
-          name="Sales"
-          isSelected={tag === "Sales"}
+          name="Public Services"
+          isSelected={tag === "Public Services"}
         />
         </div>
   
--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,10 @@ import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 import DojosSection from "./components/DojosSection";
+import DojosSection2 from "./components/DojosSection2";
+import DojosSection3 from "./components/DojosSection3";
+import DojosSection4 from "./components/DojosSection4";
+
 
 
 
@@ -20,6 +24,9 @@ export default function Home() {
         <AchievementsSection />
         <AboutSection />
         <DojosSection />
+        <DojosSection2 />
+        <DojosSection3 />
+        <DojosSection4 />
         <ProjectsSection />
         <EmailSection />
       </div>
