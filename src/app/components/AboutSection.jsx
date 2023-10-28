6146fe508152404f1a4434052a8b6b738fe3bb96
viewprox86 <workmates.live@outlook.com>
2023-10-27 19:10:35 -0500

    sd

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -105,7 +105,7 @@ const TAB_DATA = [
 ];
 
 const AboutSection = () => {
-  const [tab, setTab] = useState("uses");
+  const [tab, setTab] = useState("features");
   const [isPending, startTransition] = useTransition();
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -116,7 +116,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image style={{ borderRadius: 16}}src="/images/neon.png" width={500} height={500}   />
+        <Image style={{ borderRadius: 16}}src="/images/neon.webp" width={500} height={500}   />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">A Digital Home Away From Home</h2>
 
