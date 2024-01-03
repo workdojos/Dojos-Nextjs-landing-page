e1343ad07bb655f141d86fb1aaad14018a260fd0
viewprox86 <workmates.live@outlook.com>
2024-01-03 16:02:15 -0600

    zx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -5,7 +5,6 @@ import TabButton from "./TabButton";
 
 const TAB_DATA = [
   {
-<<<<<<< HEAD
     title: "Features",
     id: "features",
     content: (
@@ -22,23 +21,10 @@ const TAB_DATA = [
         <li>Blog</li>
 
         <li>Community collaboration</li>
-=======
-    title: "Skills",
-    id: "skills",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
-        <li>React</li>
->>>>>>> 6cc2bebf1c3a72fb97d6190b6924e8cf251ec135
       </ul>
     ),
   },
   {
-<<<<<<< HEAD
     title: "use-cases",
     id: "use-cases",
     content: (
@@ -78,24 +64,6 @@ const TAB_DATA = [
         <li>Moodle</li>
         <li>Nextcloud</li>
         <li>and more</li>
-=======
-    title: "Education",
-    id: "education",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Certifications",
-    id: "certifications",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
->>>>>>> 6cc2bebf1c3a72fb97d6190b6924e8cf251ec135
       </ul>
     ),
   },
@@ -115,7 +83,6 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-<<<<<<< HEAD
         <Image style={{ borderRadius: 16}}src="/images/spark.gif" width={500} height={500}   />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Dojos </h2>
@@ -150,40 +117,6 @@ const AboutSection = () => {
             >
               {" "}
               Use-cases{" "}
-=======
-        <Image src="/images/about-image.png" width={500} height={500} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
->>>>>>> 6cc2bebf1c3a72fb97d6190b6924e8cf251ec135
             </TabButton>
           </div>
           <div className="mt-8">
