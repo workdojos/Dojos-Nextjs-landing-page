94f98f1aa0303baeb631945eedef86c3dd61c533
workdojos <itsarectangle@gmail.com>
2024-03-24 06:46:24 -0500

    sa

--- src/app/components/Join.jsx
+++ src/app/components/Join.jsx
@@ -52,7 +52,23 @@ const HeroSection = () => {
               </span>
             </Link>
           </div>
-          <div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="lg:w-[400px] lg:h-[300px] relative ">
+            <Image
+              style={{borderRadius: 20}}
+              src="/images/winner.gif"
+              alt="A beautiful Swedish woman holding a trophy"
+              className=".img-container"
+              width={307}
+              height={307}
+            />
+          </div>
         </motion.div>
       </div>
     </section>
