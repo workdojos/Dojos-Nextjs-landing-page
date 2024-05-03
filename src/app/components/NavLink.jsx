3f43a560aa8eed6e78cf6489f85932f23756b0d8
workdojos <itsarectangle@gmail.com>
2024-05-03 15:15:27 -0500

    as

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -5,6 +5,8 @@ const NavLink = ({ href, title }) => {
     <Link
       href={href}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+      target="_blank" 
+      rel="noopener noreferrer" 
     >
       {title}
     </Link>
